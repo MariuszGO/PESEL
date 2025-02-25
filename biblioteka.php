80cc80bf818932ff6153735d7a40e525844c3c8a
Mariusz Gogola <mariuszgogola@gmail.com>
2025-02-25 13:48:16 +0100

    n111

---
+++ biblioteka.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pl-PL">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Biblioteka</title>
+</head>
+<body>
+<header></header>
+<nav></nav>
+<main></main>
+<aside></aside>
+<footer></footer>
+
+</body>
+</html>
