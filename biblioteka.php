1dd4cfb4c4243659356426631f0646b17559ba69
Mariusz <m.gogola@lukasiewicz-gorlice.pl>
2025-02-26 12:13:46 +0100

    n33

--- biblioteka.php
+++ biblioteka.php
@@ -13,7 +13,12 @@
 </header>
 <nav>
     <H4>Dodaj czytelnika</H4>
-    <
+    <form action="biblioteka.php" method="post">
+    <p>Imie: <input type="text" name="imie"> </p>
+    <label for="nazwisko">Nazwisko: </label> <input type="text" name="nazwisko" id="nazwisko"><br><br>
+    <label  for="symbol"> Symbol: </label> <input type="number" name="symbol" id="symbol"><br><br>
+    <button>Akceptuj</button>
+</form>
 </nav>
 <main></main>
 <aside></aside>
